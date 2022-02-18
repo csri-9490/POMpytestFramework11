691633bc39547897669750addece9e81b8ac8cd4
srikanth <srikanthchelukala@gmail.com>
2022-02-18 12:13:28 +0530

    Initial commit

---
+++ Config/Config.py
@@ -0,0 +1,7 @@
+class TestData:
+    chrome_executable_path="D:/Selenium/browser/chromedriver.exe"
+    firefox_executable_path="D:/Selenium/browser/geckodriver.exe"
+    BASE_URL="https://www.facebook.com/"
+    USER_NAME="reddy.csri@gmail.com"
+    PASSWORD="Csrireddy77!"
+    TITLE="Facebook – log in or sign up"
